a50aebe6c67afcdde1aea8b856c4101200bb8214
Steffe <s.schubert@folge3.de>
2022-03-13 16:21:46 +0100

    TestProject updated

--- TestProjects/MyTestProject/src/methode-nach-oben-verschieben/parent.js
+++ TestProjects/MyTestProject/src/methode-nach-oben-verschieben/parent.js
@@ -1,2 +1,33 @@
 class Parent {
 }
+
+class ChildInSameFile extends Parent {
+    constructor() {
+        super();
+        this._member = 'member';
+    }
+
+    moveThisUpBasic() {
+        console.log('Hello, world!');
+    }
+
+    moveThisUpUsingOtherFunction() {
+        this.otherFunction();
+    }
+
+    otherFunction() {
+        console.log('Hello, world!');
+    }
+
+    moveThisUpUsingOtherMember() {
+        return this.member;
+    }
+
+    get member() {
+        return this._member;
+    }
+
+    dontMoveThisUp() {
+        console.log('Hello, world! Im a ChildInSameFile');
+    }
+}
