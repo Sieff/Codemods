f0768f8ee265d3ca02c48276d4bdecf85bf63adb
Steffe <s.schubert@folge3.de>
2022-03-13 14:13:59 +0100

    Data für Methode nach oben verschieben

---
+++ TestProjects/MyTestProject/src/methode-nach-oben-verschieben/other-child.js
@@ -0,0 +1,30 @@
+class OtherChild extends Parent {
+    constructor() {
+        super();
+        this._member = 'member';
+    }
+
+    moveThisUpBasic() {
+        console.log('Hello, world!');
+    }
+
+    moveThisUpUsingOtherFunction() {
+        this.otherFunction();
+    }
+
+    otherFunction() {
+        console.log('Hello, world!');
+    }
+
+    moveThisUpUsingOtherMember() {
+        return this.member;
+    }
+
+    get member() {
+        return this._member;
+    }
+
+    dontMoveThisUp() {
+        console.log('Hello, world! Im an OtherChild');
+    }
+}
