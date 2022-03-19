e1d0e1bc332837ec2daa0869c4a65f55af0d3872
Steffe <s.schubert@folge3.de>
2022-03-19 17:29:51 +0100

    Update schleife-aufteilen-input.js

--- TestProjects/MyTestProject/src/schleife aufteilen/schleife-aufteilen-input.js
+++ TestProjects/MyTestProject/src/schleife aufteilen/schleife-aufteilen-input.js
@@ -7,6 +7,35 @@ function basic(array) {
     }
 }
 
+function crossDependent(array) {
+    let firstAccumulator = 0;
+    let secondAccumulator = 0;
+    for (const elem of array) {
+        firstAccumulator += elem.obj1;
+        secondAccumulator += elem.obj2 + firstAccumulator;
+    }
+}
+
+function crossDependentAdvanced(array) {
+    let firstAccumulator = 0;
+    let secondAccumulator = 0;
+    for (const elem of array) {
+        firstAccumulator += elem.obj1 + Math.max([0, firstAccumulator]);
+        secondAccumulator += elem.obj2 + Math.max([0, firstAccumulator]);
+    }
+}
+
+
+
+function selfDependent(array) {
+    let firstAccumulator = 0;
+    let secondAccumulator = 0;
+    for (const elem of array) {
+        firstAccumulator += elem.obj1 + firstAccumulator;
+        secondAccumulator = elem.obj2 + secondAccumulator;
+    }
+}
+
 function triple(array) {
     let firstAccumulator = 0;
     let secondAccumulator = 0;
