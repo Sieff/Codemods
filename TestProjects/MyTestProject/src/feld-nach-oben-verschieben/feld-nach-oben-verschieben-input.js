b67a6329927dd115af0e22fd8e9b0eb66838c09e
Steffe <s.schubert@folge3.de>
2022-03-25 12:39:06 +0100

    Tests hinzugefügt

---
+++ TestProjects/MyTestProject/src/feld-nach-oben-verschieben/feld-nach-oben-verschieben-input.js
@@ -0,0 +1,27 @@
+class FeldNachObenParent {
+
+}
+
+class FeldNachObenChild1 extends FeldNachObenParent {
+    constructor(field1) {
+        super();
+        this._field1 = field1 + 1;
+        this.field2 = field2;
+    }
+
+    set field2(value) {
+        this._field2 = value;
+    }
+}
+
+class FeldNachObenChild2 extends FeldNachObenParent {
+    constructor(field1, field2) {
+        super();
+        this._field1 = field1 + 1;
+        this.field2 = field2;
+    }
+
+    set field2(value) {
+        this._field2 = value;
+    }
+}
