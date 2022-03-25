2b71aa0ab0718a938c164696f0a94a0e358144dc
Steffe <s.schubert@folge3.de>
2022-03-25 17:59:03 +0100

    Variable Kapsel Testdatei

---
+++ TestProjects/MyTestProject/src/variable-kapseln/variable-kapseln-input.js
@@ -0,0 +1,62 @@
+class VariableKapselnInput {
+    constructor(field1, field2, field3, field4, field5, field6) {
+        // Don't Replace
+        this.field1 = field1;
+        // Don't Replace
+        this.field2 = field2;
+        // Don't Replace
+        this._field3 = field3;
+        // Add Getter/Setter and replace with Setter
+        this._field4 = field4;
+        // Replace with Setter
+        this._field5 = field5;
+        // Replace with Setter
+        this._field6 = field6;
+    }
+
+    // Don't add getters or setters, when there are already some! Start
+    set field1(value) {
+        this._field1 = value;
+    }
+
+    get field1() {
+        return this._field1;
+    }
+
+    set field2(value) {
+        this._field2 = value;
+    }
+
+    get field3() {
+        return this._field3;
+    }
+
+    set field5(value) {
+        this._field5 = value;
+    }
+
+    get field5() {
+        return this._field5;
+    }
+
+    set field6(value) {
+        this._field6 = value;
+    }
+    // End
+
+    usingTheFields() {
+        // Replace with Setter
+        this._field1 = 1;
+        // Replace with Setter
+        this._field2 = 2;
+        this._field3 = 3;
+        // Gets replaced with new Setter
+        this._field4 = 4;
+        // Replace with Setter
+        this._field5 = 5;
+        // Replace with Setter
+        this._field6 = 6;
+
+        const someVariable = this.field1 + this._field1 + this._field2 + this.field3 + this._field3 + this._field4
+    }
+}
