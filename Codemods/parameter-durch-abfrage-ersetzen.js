fbcbf40a844cc8dbe62cb50732193aca019d4a95
Steffe <s.schubert@folge3.de>
2022-03-26 12:46:24 +0100

    Create parameter-durch-abfrage-ersetzen.js

---
+++ Codemods/parameter-durch-abfrage-ersetzen.js
@@ -0,0 +1,9 @@
+/**
+ * Alle Calls angucken
+ * Von denen schauen ob ein Parameter von anderem abgeleitet werden kann
+ * gucken ob alle Calls so aussehen
+ * Wenn nein ciao
+ * Wenn ja variable in funktion einbauen die so heißt wie parameter
+ * und abfrage machen
+ * gucken, dass funktion auch in anderen dateien ist???
+ */
