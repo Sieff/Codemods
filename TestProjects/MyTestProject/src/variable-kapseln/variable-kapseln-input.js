30616bb904bfe54ae1c59d6fcd5d762d676361a4
Steffe <s.schubert@folge3.de>
2022-04-03 21:48:47 +0200

    Variable Kapseln Tests

--- TestProjects/MyTestProject/src/variable-kapseln/variable-kapseln-input.js
+++ TestProjects/MyTestProject/src/variable-kapseln/variable-kapseln-input.js
@@ -1,20 +1,29 @@
+/*
+ * Variable kapseln Testcases:
+ * 1. Ein Feld mit Getter und Setter, dass im Konstruktor mit Setter aufgerufen wird
+ * 2. Ein Feld mit Setter, dass im Konstruktor mit Setter aufgerufen wird
+ * 3. Ein Feld mit Getter
+ * 4. Ein Feld ohne Getter und ohne Setter
+ * 5. Ein Feld mit Getter und Setter, dass im Konstruktor direkt aufgerufen wird
+ * 6. Ein Feld mit Setter, dass im Konstruktor direkt aufgerufen wird
+ */
+
 class VariableKapselnInput {
     constructor(field1, field2, field3, field4, field5, field6) {
-        // Don't Replace
+        // Testcases 1.: Variable direkt aufrufen
         this.field1 = field1;
-        // Don't Replace
+        // Testcases 2.: Variable direkt aufrufen
         this.field2 = field2;
-        // Don't Replace
+        // Testcases 3.:
         this._field3 = field3;
-        // Add Getter/Setter and replace with Setter
+        // Testcases 4.: Getter und Setter hinzufügen
         this._field4 = field4;
-        // Replace with Setter
+        // Testcases 5.:
         this._field5 = field5;
-        // Replace with Setter
+        // Testcases 6.:
         this._field6 = field6;
     }
 
-    // Don't add getters or setters, when there are already some! Start
     set field1(value) {
         this._field1 = value;
     }
@@ -42,21 +51,21 @@ class VariableKapselnInput {
     set field6(value) {
         this._field6 = value;
     }
-    // End
 
     usingTheFields() {
-        // Replace with Property
+        // Variable statt Setter direkt aufrufen
         this.field1 = 1;
-        // Replace with Property
+        // Variable statt Setter direkt aufrufen
         this.field2 = 2;
         this._field3 = 3;
-        // Gets replaced with new Property
+        // Neue interne Variable aufrufen
         this._field4 = 4;
-        // Replace with Property
+        // Variable statt Setter direkt aufrufen
         this.field5 = 5;
-        // Replace with Property
+        // Variable statt Setter direkt aufrufen
         this.field6 = 6;
 
+        // Variablen direkt aufrufen
         const someVariable = this.field1 + this._field1 + this._field2 + this.field3 + this._field3 + this._field4
     }
 }
